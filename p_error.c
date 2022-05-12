a208b22f04e3f5b9ae4f4724cac3af2c080f97d9
David <4292@holbertonschool.com>
2022-05-12 16:40:54 -0400

    removed <>

--- p_error.c
+++ p_error.c
@@ -12,17 +12,17 @@ void p_error(int count, char *arg1)
 	extern int val;
 
 	if (val == -1)
-		fprintf(stderr, "L<%d>: usage: push integer\n", count);
+		fprintf(stderr, "L%d: usage: push integer\n", count);
 	if (val == -2)
-		fprintf(stderr, "L<%d>: unknown instruction <%s>\n", count, arg1);
+		fprintf(stderr, "L%d: unknown instruction %s\n", count, arg1);
 	if (val == -3)
 		fprintf(stderr, "Error: malloc failed\n");
 	if (val == -4)
-		fprintf(stderr, "L<%d>: can't pint, stack empty\n", count);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", count);
 	if (val == -5)
-		fprintf(stderr, "L<%d>: can't pop an empty stack\n", count);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", count);
 	if (val == -6)
-		fprintf(stderr, "L<%d>: can't swap, stack too short\n", count);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", count);
 	if (val == -7)
-		fprintf(stderr, "L<%d>: can't add, stack too short\n", count);
+		fprintf(stderr, "L%d: can't add, stack too short\n", count);
 }
